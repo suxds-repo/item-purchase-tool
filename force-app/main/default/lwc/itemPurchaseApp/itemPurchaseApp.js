9bf5f532ecc73111136147aa7348733dadb2a729
suxds <suxds@yandex.ru>
2026-02-11 15:31:28 +0300

    Added fliter and search for Name and desc, Type, Family. Addedd modal window for Details of Item

--- force-app/main/default/lwc/itemPurchaseApp/itemPurchaseApp.js
+++ force-app/main/default/lwc/itemPurchaseApp/itemPurchaseApp.js
@@ -1,5 +1,6 @@
 import { LightningElement, wire, track } from 'lwc';
 import { CurrentPageReference } from 'lightning/navigation';
+import getItems from '@salesforce/apex/ItemPurchaseController.getItems';
 
 import getAccount from '@salesforce/apex/ItemPurchaseController.getAccount';
 
@@ -7,6 +8,26 @@ export default class ItemPurchaseApp extends LightningElement {
 
     @track accountId;
     @track account;
+    @track items = [];
+    familyOptions = [
+        { label: '--All--', value: '' },
+        { label: 'A', value: 'A' },
+        { label: 'B', value: 'B' },
+        { label: 'C', value: 'C' },
+        { label: 'D', value: 'D' },
+    ];
+
+    typeOptions = [
+        { label: '--All--', value: '' },
+        { label: 'Food', value: 'Food' },
+        { label: 'Tech', value: 'Tech' },
+        { label: 'Office', value: 'Office' },
+        { label: 'Other', value: 'Other' },
+    ];
+
+    @track familyFilter = null;
+    @track typeFilter = null;
+    @track searchKey = null;
 
     @wire(CurrentPageReference)
     getStateParameters(pageRef) {
@@ -27,4 +48,53 @@ export default class ItemPurchaseApp extends LightningElement {
             console.error(error);
         }
     }
+
+    @wire(getItems, {
+        family: '$familyFilter',
+        type: '$typeFilter',
+        searchKey: '$searchKey'
+    })
+    wiredItems({ error, data }) {
+
+        if (data) {
+            this.items = data;
+        }
+
+        if (error) {
+            console.error(error);
+        }
+    }
+
+
+    handleSearch(event) {
+        this.searchKey = event.target.value;
+    }
+
+    handleFamily(event) {
+        this.familyFilter = event.target.value;
+    }
+
+    handleType(event) {
+        this.typeFilter = event.target.value;
+    }
+
+
+    @track showModal = false;  
+    @track selectedItem;     
+
+    handleDetails(event) {
+        const itemId = event.target.dataset.id;
+
+        this.selectedItem = this.items.find(i => i.Id === itemId);
+
+        if (this.selectedItem) {
+            this.showModal = true;
+        }
+    }
+
+    handleCloseModal() {
+        this.showModal = false;
+        this.selectedItem = null;
+    }
+
 }
