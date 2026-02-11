cee65a5183b2e217eb3c4cc0ae4fe1099d280d4c
suxds <suxds@yandex.ru>
2026-02-11 13:57:42 +0300

    Added redirect to Item Purchase Tool Page from an Account page. Account Name, Number and Account Industry are visible on new page.

--- force-app/main/default/lwc/itemPurchaseApp/itemPurchaseApp.js
+++ force-app/main/default/lwc/itemPurchaseApp/itemPurchaseApp.js
@@ -1,3 +1,30 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire, track } from 'lwc';
+import { CurrentPageReference } from 'lightning/navigation';
 
-export default class ItemPurchaseApp extends LightningElement {}
+import getAccount from '@salesforce/apex/ItemPurchaseController.getAccount';
+
+export default class ItemPurchaseApp extends LightningElement {
+
+    @track accountId;
+    @track account;
+
+    @wire(CurrentPageReference)
+    getStateParameters(pageRef) {
+
+        if (pageRef && pageRef.state?.c__accountId) {
+            this.accountId = pageRef.state.c__accountId;
+        }
+    }
+
+    @wire(getAccount, { accountId: '$accountId' })
+    wiredAccount({ error, data }) {
+
+        if (data) {
+            this.account = data;
+        }
+
+        if (error) {
+            console.error(error);
+        }
+    }
+}
